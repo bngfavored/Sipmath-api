657f5aaac04ac1528a45bf7056e2e30f6183c62a
Richard Chevious <bngfavored@gmail.com>
2022-12-23 12:00:56 -0600

    Added Seeds as optional value

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ class SIPDataModel(BaseModel):
     boundedness: List[str]
     bounds: List[List[Union[int, float]]]
     term_saved: List[int]
+    seeds: Optional[List[List[Union[int, float, str]]]] = None
     probs: List[List[float]]
 
 @app.post("/sipmath-json")
@@ -28,11 +29,23 @@ def sipmath_json(sip_data: SIPDataModel):
     data_dict_for_JSON = dict(boundedness=sip_data.boundedness,
                     bounds=sip_data.bounds,
                     term_saved=sip_data.term_saved)
+    seed_list = [
+                    {   'name':f'hdr{i}',
+                        'function':'HDR_2_0',
+                        'arguments':{
+                            'counter':'PM_Index',
+                            'entity': current_seeds[0],
+                            'varId': current_seeds[1],
+                            'seed3': current_seeds[2],
+                            'seed4': current_seeds[3]}
+                            } for i, current_seeds in enumerate(sip_data.seeds, 1)
+                ] if sip_data.seeds else []
     PySIP.Json(SIPdata=df,
                     file_name=filename,
                     author=sip_data.author,
                     dependence=sip_data.dependence,
                     setupInputs=data_dict_for_JSON,
+                    seeds=seed_list,
                     probs=sip_data.probs)
     with open(filename, 'rb') as f:
         return json.load(f)
